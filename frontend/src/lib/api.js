b142384c9f73e99b30936b368b8f9d397c1d5a27
Eisatopon <sokkorom@gmail.com>
2026-09-08 06:45:09 +0300

    Restore production API fallback

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = `${process.env.REACT_APP_BACKEND_URL}/api`;
+const API = `${process.env.REACT_APP_BACKEND_URL || "https://platforma-mathimatikon.onrender.com"}/api`;
 
 export const fetchGrades = () => axios.get(`${API}/grades`).then((r) => r.data);
 export const fetchAllLessons = () => axios.get(`${API}/lessons`).then((r) => r.data);
